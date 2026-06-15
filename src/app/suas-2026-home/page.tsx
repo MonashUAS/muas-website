562e633b3f146517cad87b8afea6a8ba69043486
Claire Zhang <clairez2015@gmail.com>
2026-06-16 00:49:08 +1000

    add technical specs and team link to homepage

--- src/app/suas-2026-home/page.tsx
+++ src/app/suas-2026-home/page.tsx
@@ -1,13 +1,18 @@
 import { ScrollHero } from "./ScrollHero";
 import { KeyFeatures } from "./KeyFeatures";
 import { Video } from "./Video";
+import { TechSpecs } from "./TechSpecs";
+import { TeamLink } from "./TeamLink";
 
+// SUAS2026HomePage composes the full public-facing Redback home page.
 export default function SUAS2026HomePage() {
   return (
     <div className="bg-black-500">
       <ScrollHero />
       <Video />
       <KeyFeatures />
+      <TechSpecs />
+      <TeamLink />
     </div>
   );
 }
